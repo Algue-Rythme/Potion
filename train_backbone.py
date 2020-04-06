acaf4367f3226247765e79ec5266bc58781166de
Algue-Rythme <algue-rythme@orange.fr>
2020-04-06 19:11:10 +0200

    first commit

---
+++ train_backbone.py
@@ -0,0 +1,243 @@
+import argparse
+import collections
+import csv
+import os
+from os import path
+
+import numpy as np
+import torch
+from torch.autograd import Variable
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import tqdm
+
+from datamgr import SimpleDataManager, SetDataManager
+from wrn import wrn28_10
+from io_utils import parse_args, get_resume_file, get_assigned_file
+
+
+use_gpu = torch.cuda.is_available()
+
+
+def get_rotations_triplet(inputs, targets, split_ratio=None):
+    batch_size = inputs.size(0)
+    rotated_inputs = []
+    rotated_targets = []
+    angles_indexes = []
+    indices = np.arange(batch_size)
+    if split_ratio is not None:
+        np.random.shuffle(indices)
+        split_size = int(batch_size * split_ratio)
+        indices = indices[:split_size]
+    for j in indices:
+        x90 = inputs[j].transpose(2,1).flip(1)
+        x180 = x90.transpose(2,1).flip(1)
+        x270 =  x180.transpose(2,1).flip(1)
+        rotated_inputs += [inputs[j], x90, x180, x270]
+        rotated_targets += [targets[j] for _ in range(4)]
+        angles_indexes += list(range(4))
+    rotated_inputs = torch.stack(rotated_inputs)
+    rotated_targets = torch.stack(rotated_targets)
+    angles_indexes = torch.LongTensor(angles_indexes)
+    if use_gpu:
+        rotated_inputs = rotated_inputs.cuda()
+        rotated_targets = rotated_targets.cuda()
+        angles_indexes = angles_indexes.cuda()
+    return rotated_inputs, rotated_targets, angles_indexes
+
+def mixup_criterion(criterion, pred, y_a, y_b, lam):
+        return lam * criterion(pred, y_a) + (1 - lam) * criterion(pred, y_b)
+
+def evaluate(base_loader_test, model, rotate_classifier=None):
+    model.eval()
+    if rotate_classifier is not None:
+        rotate_classifier.eval()
+    with torch.no_grad():
+        test_loss = 0
+        correct, total = 0, 0
+        for batch_idx, (inputs, targets) in enumerate(base_loader_test):
+            if use_gpu:
+                inputs, targets = inputs.cuda(), targets.cuda()
+            
+            if rotate_classifier is not None:
+                triplet = get_rotations_triplet(inputs, targets, split_ratio=None)
+                inputs, targets, angles_indexes = triplet
+
+            out_latent, outputs = model.forward(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.data.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += targets.size(0)
+            correct += predicted.eq(targets.data).cpu().sum()
+
+            if rotate_classifier is not None:
+                rotate_logits = rotate_classifier(out_latent)
+                rotate_predicted = torch.argmax(rotate_logits, 1)
+                rotate_correct += (rotate_predicted==angles_indexes).sum().item()
+        
+        print("Epoch {0} : Accuracy {1}".format(epoch, (float(correct)*100)/total), end='')
+        if rotate_classifier is not None:
+            print('') # new line
+        else:
+            print(" : Rotate Accuracy {2}".format(float(rotate_correct)*100)/total)
+    torch.cuda.empty_cache()  # ?
+
+
+def update_batch_infos(progress, classifier_losses, rotate_losses, correct, total):
+    avg_train_loss = np.mean(classifier_losses)
+    avg_rot_loss = np.mean(rotate_losses)
+    acc = 100.*correct/total
+    desc =  'Loss: %.3f | Acc: %.3f%% | RotLoss: %.3f '%(avg_train_loss, acc, avg_rot_loss)
+    progress.set_description(desc=desc)
+    progress.update()
+
+
+def train_s2m2(base_loader, base_loader_val, model, start_epoch, stop_epoch, params, fine_tuning):
+    cross_entropy = nn.CrossEntropyLoss()
+    criterion = nn.CrossEntropyLoss()
+
+    rotate_classifier = nn.Sequential(nn.Linear(model.final_feat_dim, 4))
+    if use_gpu:
+        rotate_classifier.cuda()
+
+    optimizer = torch.optim.Adam([
+                {'params': model.parameters()},
+                {'params': rotate_classifier.parameters()}
+            ])
+
+    print("stop_epoch", start_epoch, stop_epoch)
+
+    MixupTuple = collections.namedtuple('MixupParams', 'targets hidden input_space alpha lam')
+
+    for epoch in range(start_epoch, stop_epoch):
+        print('\nEpoch: %d' % epoch)
+
+        model.train()
+        rotate_classifier.train()
+        classifier_losses, rotation_losses = [], []
+        correct, total = 0, 0
+
+        if use_gpu:
+            torch.cuda.empty_cache()
+        
+        progress = tqdm.tqdm(total=len(base_loader), leave=True, desc='training')
+        for batch_idx, (inputs, targets) in enumerate(base_loader):
+
+            optimizer.zero_grad()
+            
+            if use_gpu:
+                inputs, targets = inputs.cuda(), targets.cuda()
+
+            if fine_tuning:
+                lam = np.random.beta(params.alpha, params.alpha)
+                mixup_params = MixupTuple(targets=targets, hidden=True, alpha=params.alpha, lam=lam)
+                _, outputs, target_a, target_b = model(inputs, mixup_params)
+                loss = mixup_criterion(criterion, outputs, target_a, target_b, lam)
+                classifier_losses.append(loss.item())
+                loss.backward()  # Mixup loss
+
+                _, predictions = torch.max(outputs, 1)
+                total += targets.size(0)
+                correct += (lam * predictions.eq(target_a).cpu().sum().float()
+                            + (1 - lam) * predictions.eq(target_b).cpu().sum().float())
+
+            rotations_split_ratio = 0.25 if fine_tuning else 1.  # less rotations with fine tuning
+            triplet = get_rotations_triplet(inputs, targets, split_ratio=rotations_split_ratio)
+            inputs, targets, angles_indexes = triplet
+
+            latent_space, outputs = model(inputs)
+            rotate_outputs = rotate_classifier(latent_space)
+
+            rotation_loss = cross_entropy(rotate_outputs, angles_indexes)
+            classifier_loss = criterion(outputs, targets)  # supervised loss (softmax, cosine... )
+            loss = rotation_loss + classifier_loss
+
+            rotation_losses.append(rotation_loss.item())
+            if not fine_tuning:
+                _, predictions = torch.max(outputs, 1)
+                correct += predictions.eq(targets).cpu().sum().float()
+                total += targets.size(0)
+                classifier_losses.append(classifier_loss.item())
+            
+            loss.backward()  # Rotation loss + Cosine Loss
+            optimizer.step()
+
+            update_batch_infos(progress, classifier_losses, rotation_losses, correct, total)
+
+        if not os.path.isdir(params.checkpoint_dir):
+            os.makedirs(params.checkpoint_dir)
+
+        if (epoch % params.save_freq==0) or (epoch==stop_epoch-1):
+            outfile = os.path.join(params.checkpoint_dir, '{:d}.tar'.format(epoch))
+            torch.save({'epoch':epoch, 'state':model.state_dict() }, outfile)
+
+        if fine_tuning:
+            evaluate(base_loader_val, model, rotate_classifier=None)  # do not check rotations anymore
+        else:
+            evaluate(base_loader_val, model, rotate_classifier=rotate_classifier)
+       
+    return model 
+
+
+def resume_training(checkpoint_dir, model):
+    resume_file = get_resume_file(params.checkpoint_dir )        
+    print("resume_file", resume_file)
+    tmp = torch.load(resume_file)
+    start_epoch = tmp['epoch']+1
+    print("restored epoch is" , tmp['epoch'])
+    state = tmp['state']                 
+    model.load_state_dict(state)
+    return start_epoch
+
+def enable_gpu_usage(model):
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model, device_ids = range(torch.cuda.device_count()))  
+    model.cuda()
+    return model
+
+
+if __name__ == '__main__':
+    params = parse_args('train')
+
+    image_size = 84
+    # the weights are stored into ./weights folder
+    save_dir = './weights'
+    # the location of the json files, themselves containing the location of the images
+    data_dir = {}
+    data_dir['cifar']           = './filelists/cifar/' 
+    data_dir['CUB']             = './filelists/CUB/' 
+    data_dir['miniImagenet']    = './filelists/miniImagenet/' 
+
+    base_file = data_dir[params.dataset] + 'base.json'
+    val_file = data_dir[params.dataset] + 'val.json'
+    params.checkpoint_dir = '%s/checkpoints/%s/%s_%s' %(save_dir, params.dataset, params.model, params.method)
+    start_epoch = params.start_epoch
+    stop_epoch = params.stop_epoch
+
+    base_datamgr = SimpleDataManager(image_size, batch_size=params.batch_size)
+    base_loader = base_datamgr.get_data_loader(base_file, aug=params.train_aug)
+    base_datamgr_val = SimpleDataManager(image_size, batch_size=params.test_batch_size)
+    base_loader_val = base_datamgr_val.get_data_loader(val_file, aug=False)
+
+    if params.model == 'WideResNet28_10':
+        model = wrn28_10(num_classes=params.num_classes)
+    else:
+        raise ValueError
+
+    if use_gpu:
+        model = enable_gpu_usage(model)
+
+    if params.resume:
+        start_epoch = resume_training(params.checkpoint_dir, model)
+    elif params.method =='S2M2_R':
+        resume_rotate_file_dir = params.checkpoint_dir.replace("S2M2_R","rotation")
+        start_epoch = resume_training(resume_rotate_file_dir, model)
+    
+    if params.method =='S2M2_R':
+        model = train_s2m2(base_loader, base_loader_val, model, start_epoch, start_epoch+stop_epoch, params, fine_tuning=True)
+    elif params.method =='rotation':
+        model = train_s2m2(base_loader, base_loader_val, model, start_epoch, stop_epoch, params, fine_tuning=False)
---
+++ wrn.py
@@ -0,0 +1,184 @@
+# This code is modified from https://github.com/nupurkmr9/S2M2_fewshot
+# S2M2_fewshot is itself derived from https://github.com/wyharveychen/CloserLookFewShot
+# but lacks proper references to it, including the license
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import sys, os
+import numpy as np
+import random
+
+import math
+from torch.nn.utils.weight_norm import WeightNorm
+
+
+class distLinear(nn.Module):
+    def __init__(self, indim, outdim):
+        super(distLinear, self).__init__()
+        self.L = nn.Linear(indim, outdim, bias=False)
+        self.class_wise_learnable_norm = True # If False, then it is Cosine Classifier, else it is Linear+Crossentropy
+        if self.class_wise_learnable_norm:
+            pass#WeightNorm.apply(self.L, 'weight', dim=0) # split the weight update component to direction and norm      
+        if outdim <= 200:
+            self.scale_factor = 2; # a fixed scale factor to scale the output of cos value into a reasonably large input for softmax
+        else:
+            self.scale_factor = 10; # in omniglot, a larger scale factor is required to handle >1000 output classes.
+
+    def forward(self, x):
+        epsilon = 0.00001
+        x_norm = torch.norm(x, p=2, dim=1).unsqueeze(1).expand_as(x)
+        x_normalized = x / (x_norm + epsilon)
+        if not self.class_wise_learnable_norm:
+            L_norm = torch.norm(self.L.weight.data, p=2, dim=1).unsqueeze(1).expand_as(self.L.weight.data)
+            self.L.weight.data = self.L.weight.data.div(L_norm + epsilon)
+        # matrix product by forward function, but when using WeightNorm,
+        # this also multiply the cosine distance by a class-wise learnable norm
+        cos_dist = self.L(x_normalized)
+        scores = self.scale_factor * cos_dist
+        return scores
+
+class BasicBlock(nn.Module):  # Residual Block
+    def __init__(self, in_planes, out_planes, stride, dropRate=0.0):
+        super(BasicBlock, self).__init__()
+        # there is 6 layers in Basic Block
+        self.bn1 = nn.BatchNorm2d(in_planes)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.conv1 = nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_planes)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_planes, out_planes, kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.droprate = dropRate
+        self.equalInOut = (in_planes == out_planes)
+        self.convShortcut = None if self.equalInOut else nn.Conv2d(in_planes, out_planes,
+                                                                   kernel_size=1, stride=stride,
+                                                                   padding=0, bias=False)
+    
+    def forward(self, x):
+        out = self.relu1(self.bn1(x))
+        if not self.equalInOut:
+            x = out  # x will be pass through a convolution so we ReLU + BN before
+        out = self.relu2(self.bn2(self.conv1(out)))
+        if self.droprate > 0:
+            out = F.dropout(out, p=self.droprate, training=self.training)
+        out = self.conv2(out)
+        if not self.equalInOut:
+            return out + self.convShortcut(x)  # we can sum it (same size !)
+        return out + x  # final conv not required (already same size !)
+
+class NetworkBlock(nn.Module):
+    def __init__(self, num_layers_per_block, in_planes, out_planes, block, stride, dropRate=0.0):
+        super(NetworkBlock, self).__init__()
+        self.layer = self._make_layer(block, in_planes, out_planes, num_layers_per_block, stride, dropRate)
+    def _make_layer(self, block, in_planes, out_planes, num_layers_per_block, stride, dropRate):
+        layers = []
+        for i in range(int(num_layers_per_block)):
+            if i == 0:
+                layers.append(block(in_planes, out_planes, stride, dropRate)) # strides to reduce input size
+            else:
+                layers.append(block(out_planes, out_planes, 1, dropRate))  # residual blocks with same size
+        return nn.Sequential(*layers)
+    def forward(self, x):
+        return self.layer(x)
+
+def to_one_hot(inp, num_classes):
+    y_onehot = torch.FloatTensor(inp.size(0), num_classes)
+    if torch.cuda.is_available():
+        y_onehot = y_onehot.cuda()
+    y_onehot.zero_()
+    x = inp.type(torch.LongTensor)
+    if torch.cuda.is_available():
+        x = x.cuda()
+    x = torch.unsqueeze(x, 1)
+    y_onehot.scatter_(1, x , 1)
+    return y_onehot
+
+def mixup_data(x, y, lam):
+    '''Compute the mixup data. Return mixed inputs, pairs of targets, and lambda'''
+    batch_size = x.size()[0]
+    index = torch.randperm(batch_size)
+    if torch.cuda.is_available():
+        index = index.cuda()
+    mixed_x = lam * x + (1 - lam) * x[index,:]
+    y_a, y_b = y, y[index]
+    return mixed_x, y_a, y_b, lam
+
+class WideResNet(nn.Module):
+    def __init__(self, depth=28, widen_factor=10, num_classes=200 , loss_type='dist', per_img_std=False, strides=[1,2,2]):
+        super(WideResNet, self).__init__()
+        dropRate = 0.5
+        flatten = True
+        num_blocks = 3
+        nChannels = [16, 16*widen_factor, 32*widen_factor, 64*widen_factor]
+        # We remove 4 layers (first conv, final ReLU, final BN, final Linear)
+        # Then we divise by the number of layers per basic block
+        assert((depth - 4) % 6 == 0)
+        num_layers_per_block = (depth - 4) / 6
+        block = BasicBlock
+        # 1st conv before any network block
+        self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1,
+                               padding=1, bias=False)
+        self.blocks = nn.ModuleList([NetworkBlock(num_layers_per_block, nChannels[i], nChannels[i+1],
+                                     block, strides[i], dropRate) for i in range(num_blocks)])
+        # global average pooling and linear
+        self.bn1 = nn.BatchNorm2d(nChannels[-1])
+        self.relu = nn.ReLU(inplace=True)
+        self.nChannels = nChannels[-1]
+        
+        if loss_type == 'softmax':
+            self.linear = nn.Linear(nChannels[-1], int(num_classes))
+            self.linear.bias.data.fill_(0)  # wtf ?!?!
+        elif loss_type == 'dist':
+            self.linear = distLinear(nChannels[-1], int(num_classes))
+        
+        self.num_classes = num_classes
+        if flatten:
+            self.final_feat_dim = nChannels[-1]
+        self.init_weights()
+        
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))  # custom weight initialization
+            elif isinstance(m, nn.BatchNorm2d):  # initialized assuming centered-reduced data
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def forward(self, x, mixup=None):
+        out = x
+
+        layer_mix = None
+        if mixup is not None:
+            if mixup.hidden:
+                layer_mix = random.randint(0,3)  # mixup the manifold in latent space
+            if layer_mix == 0 or mixup.input_space:  # mixup the images in input
+                out, target_a, target_b, lam  = mixup_data(out, mixup.target, lam=mixup.lam)          
+
+        # forward pass
+        out = self.conv1(out)
+        for depth, block in enumerate(self.blocks):
+            out = block(out)  # block of ResNet
+            if depth+1 == layer_mix:  # time to mixup, boys
+                out, target_a, target_b, lam = mixup_data(out, target, lam=lam)
+        out = self.relu(self.bn1(out))
+        out = F.avg_pool2d(out, out.size()[2:])
+        out_latent = out.view(out.size(0), -1)
+        output = self.linear(out_latent)
+
+        if mixup is not None:
+            return out_latent, output, target_a , target_b
+        else:
+            return out_latent, output
+                  
+        
+def wrn28_10(num_classes=200, loss_type='dist'):
+    model = WideResNet(depth=28, widen_factor=2, num_classes=num_classes,
+                       loss_type=loss_type, per_img_std=False, strides=[1, 2, 2])
+    return model
+
